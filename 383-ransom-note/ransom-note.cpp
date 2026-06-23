73a49f601d32643548963e1400a73c562f03ae7f
akarshyadav111 <yadavakarsh22@gmail.com>
2026-06-23 23:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (48.55%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool canConstruct(string r, string m) {
-        unordered_map<char, int> freq;
-        for(int i=0; i<m.length(); i++){
-            freq[m[i]]++;
+        // unordered_map<char, int> freq;
+        // for(int i=0; i<m.length(); i++){
+        //     freq[m[i]]++;
+        // }
+        // for(int i=0; i<r.length(); i++){
+        //     if(freq[r[i]]==0){
+        //         return false;
+        //     }
+        //     freq[r[i]]--;
+        // }
+        // return true;
+
+        int freq[26] = {0};
+        for(char c : m) {
+            freq[c - 'a']++;
         }
-        for(int i=0; i<r.length(); i++){
-            if(freq[r[i]]==0){
+        for(char c : r) {
+            if(freq[c - 'a'] == 0) {
                 return false;
             }
-            freq[r[i]]--;
+            freq[c - 'a']--;
         }
         return true;
     }
