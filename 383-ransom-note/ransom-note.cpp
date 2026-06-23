352ad90ef0e60dbd899bcc3cc154108cd5c87dbf
akarshyadav111 <yadavakarsh22@gmail.com>
2026-06-23 23:42:46 +0530

    Time: 7 ms (23.16%) | Memory: 11.8 MB (48.55%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     bool canConstruct(string r, string m) {
         unordered_map<char, int> freq;
-        for(int i=0; i<r.length(); i++){
-            freq[r[i]]++;
-        }
         for(int i=0; i<m.length(); i++){
-            freq[m[i]]--;
+            freq[m[i]]++;
         }
-        for(auto it : freq){
-            if(it.second > 0){
+        for(int i=0; i<r.length(); i++){
+            if(freq[r[i]]==0){
                 return false;
             }
+            freq[r[i]]--;
         }
         return true;
     }
