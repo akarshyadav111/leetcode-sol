5d7655089cee5f976c1881e97f09b66156281e6e
akarshyadav111 <yadavakarsh22@gmail.com>
2026-06-24 21:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,25 +1,62 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int low=0, high=nums.size()-1;
+        while(low<=high){
+            int mid= low + (high - low)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[low] <= nums[mid]){
+            }else if(nums[low]<=nums[mid]){
                 if(nums[low] <= target && target<=nums[mid]){
                     high=mid-1;
                 }else{
                     low=mid+1;
                 }
             }else{
-                if(nums[mid] <= target && target <= nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
                     low=mid+1;
                 }else{
                     high=mid-1;
                 }
             }
         }
+
+        // bool f = false;
+        // for(int i=0; i<nums.size()-1; i++){
+        //     if(nums[i] > nums[i+1]){
+        //         index = i;
+        //         f = true;
+        //         break;
+        //     }
+        // }
+        // int low=0, high=nums.size()-1;
+        // if(nums[index]==target){
+        //     return index;
+        // }else if(nums[index] > target){
+        //     low=index+1;
+        //     while(low<=high){
+        //         int mid = low + (high - low)/2;
+        //         if(nums[mid]==target){
+        //             return mid;
+        //         }else if(nums[mid] > target){
+        //             high=mid-1;
+        //         }else{
+        //             low=mid+1;
+        //         }
+        //     }
+        // }else{
+        //     high=index-1;
+        //     while(low<=high){
+        //         int mid = low + (high - low)/2;
+        //         if(nums[mid]==target){
+        //             return mid;
+        //         }else if(nums[mid] > target){
+        //             high=mid-1;
+        //         }else{
+        //             low=mid+1;
+        //         }
+        //     }
+        // }
         return -1;
     }
 };
