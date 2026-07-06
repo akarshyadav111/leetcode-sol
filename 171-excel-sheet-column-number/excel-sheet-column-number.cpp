2954d9cac9cc6184f4c44087cd2a6e69a837ae58
akarshyadav111 <yadavakarsh22@gmail.com>
2026-07-07 00:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (8.50%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -3,23 +3,8 @@ public:
     int titleToNumber(string s) {
         int sum=0, x=0;
         for(int i=s.length()-1; i>=0; i--){
-            // sum += pow((s[i]-'A' + 1), x++);
             sum += pow(26, x++) * (s[i] - 'A' + 1);
-            // 26^x*s[i];
         }
-        // while(i>=0){
-        //     if(i==s.length()-1){
-        //         sum += s[i--] - 'B';
-        //     }else{
-        //         sum+= 26 * s[i--] - 'B';
-        //     }
-        // }
-        // for(int i=0; i<s.length(); i++){
-        //     if(i!=0){
-        //         sum+=26;
-        //     }
-        //     sum += s[i] - 'A';
-        // }
         return sum;
     }
 };
