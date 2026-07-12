9cda0e05a209fff18434fbf93ae220e5e30c995f
akarshyadav111 <yadavakarsh22@gmail.com>
2026-07-12 14:39:26 +0530

    Time: 74 ms (38.63%) | Memory: 82.4 MB (46.64%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void backtrack(int n, int index, int k){
+        if(temp.size()==k) ans.push_back(temp);
+        for(int i=index; i<=n; i++){
+            temp.push_back(i);
+            backtrack(n, i+1, k);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtrack(n, 1, k);
+        return ans;
+    }
+};
