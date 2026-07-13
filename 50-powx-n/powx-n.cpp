03b1618c6a42a16aa3ae802a62bb63964a0bac7a
akarshyadav111 <yadavakarsh22@gmail.com>
2026-07-13 10:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (6.59%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // double p(double x, double n, double m){
+
+    // }
+    double myPow(double x, int n) {
+        // if(n==0) return x;
+        // if(n>0) n--;
+        // else n++;
+        // return myPow(, n);
+        return pow(x, n);
+    }
+};
