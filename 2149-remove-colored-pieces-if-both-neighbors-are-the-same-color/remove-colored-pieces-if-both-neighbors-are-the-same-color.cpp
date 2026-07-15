d71c82148a95f1e2b43f28d954a4d405766040d9
akarshyadav111 <yadavakarsh22@gmail.com>
2026-07-15 09:35:45 +0530

    Time: 11 ms (14.68%) | Memory: 15.5 MB (96.70%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -3,8 +3,10 @@ public:
     bool winnerOfGame(string c) {
         int c1{}, c2{};
         for(int i=1; i<c.length()-1; i++){
-            if(c[i] == 'A' && c[i-1] == 'A' && c[i+1] == 'A') c1++;
-            if(c[i] == 'B' && c[i-1] == 'B' && c[i+1] == 'B') c2++;
+            if(c[i] == c[i-1] && c[i+1] == c[i]){
+                if(c[i] == 'A') c1++;
+                else c2++;
+            }
         }
         return c1 > c2;
     }
