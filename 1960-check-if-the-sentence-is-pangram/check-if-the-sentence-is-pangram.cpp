18fb2574ce17c83e9f44150610f1dc8f7ae52345
akarshyadav111 <yadavakarsh22@gmail.com>
2026-07-24 23:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (35.70%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        int count = 0;
-        vector<bool> found(100, false);
-        for(int i=0; i<sentence.length(); i++){
-            if(!found[sentence[i]]){
-                count++;
-                found[sentence[i]] = true;
-            }
+        // int count = 0;
+        // vector<bool> found(100, false);
+        // for(int i=0; i<sentence.length(); i++){
+        //     if(!found[sentence[i]]){
+        //         count++;
+        //         found[sentence[i]] = true;
+        //     }
+        // }
+        // if(count == 26){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        unordered_set<char> s;
+        for(size_t i=0; i<sentence.length(); i++){
+            s.insert(sentence[i]);
         }
-        if(count == 26){
-            return true;
-        }else{
-            return false;
-        }
+        return s.size() == 26;
     }
 };
