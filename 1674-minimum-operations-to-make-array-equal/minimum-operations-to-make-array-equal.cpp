ccddc73d62a7da81db37d46a2eb79a2cf7e9d6da
akarshyadav111 <yadavakarsh22@gmail.com>
2026-08-27 22:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (76.54%) - LeetSync

---
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int arr[n];
+        for(int i=0; i<n; i++){
+            arr[i] = (2 * i) + 1;
+        }
+        int m = (arr[0] + arr[n-1])/2;
+        int c{};
+        for(int i=0; i<n; i++){
+            c+= abs(arr[i] - m);
+        }
+        return c/2;
+    }
+};
