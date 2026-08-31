de311e1720ea012d5437a3bfe7ca70aa101e7bf7
akarshyadav111 <yadavakarsh22@gmail.com>
2026-09-01 01:45:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (68.61%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
@@ -7,10 +7,6 @@ public:
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 row[i] = max(row[i], grid[i][j]);
-            }
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
                 col[j] = max(col[j], grid[i][j]);
             }
         }
